81fd3046bdb3a6bf81f4d3ce2de3e04728534070
sonud4m <131610711+sonud4m@users.noreply.github.com>
2023-04-26 02:01:52 +0900

    プレイヤーアニメーションの実装
    ジャンプ時のアニメーションとPlayer.gdのリファクタリング

--- Godot-Test/GodotTest/Player.gd
+++ Godot-Test/GodotTest/Player.gd
@@ -9,7 +9,20 @@ const JUMP_POWER := -400
 #移動速度
 var velocity := Vector2()
 
+#ジャンプできるかどうか
+var can_jump := true
+
+#スプライト
+onready var sprite := $sprite
+
 func _process(delta):
+	#移動処理を行う
+	_update_moving(delta)
+	
+	#アニメーション処理
+	_update_anim(delta)
+	
+func _update_moving(delta):
 	#重力を計算
 	velocity.y += GRAVITY_POWER * delta
 	
@@ -26,3 +39,10 @@ func _process(delta):
 		velocity.y = JUMP_POWER
 	#移動処理
 	move_and_collide(velocity * delta)
+	
+func _update_anim(delta):
+	#基本
+	sprite.frame = 0
+	if velocity.y < 0:
+	#上昇中
+		sprite.frame = 1
